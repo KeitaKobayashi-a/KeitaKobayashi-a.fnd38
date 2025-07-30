ad09f4529bfe28374234050d6d63978347afd40d
KeitaKobayashi-a <keita02kobayashi@gmail.com>
2025-07-30 13:16:21 +0900

    Update tokyo.js

--- tokyo.js
+++ tokyo.js
@@ -22,12 +22,12 @@ myform.onsubmit = function (e) {
   let inputForm = myform.userInput.value;
 
   if (inputForm === stationsRoman[idx]) {
-    new Audio("audio/drum.mp3").play();
+    new Audio("drum.mp3").play();
     idx++;
     showStationName(idx);
     document.getElementById("userInput").value = "";
   } else {
     document.getElementById("userInput").value = "";
-    new Audio("audio/miss.mp3").play();
+    new Audio("miss.mp3").play();
   }
 };
